bba555851f4ffe0c61dd1263c3d3d9a6e388b184
achoma <139071702+achoma@users.noreply.github.com>
2024-01-29 23:19:05 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+const handleClick = (event) => {
+  const bgColor = getRandomHexColor();
+  document.body.style.backgroundColor = bgColor;
+  color.textContent = bgColor;
+};
+
+button.addEventListener("click", handleClick);
