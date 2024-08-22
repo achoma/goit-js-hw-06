a178ba46e6766666d13de7b8c9ef6d21a3f8dbce
achoma <139071702+achoma@users.noreply.github.com>
2024-08-23 01:08:10 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,27 @@
-class User {
-  // Nieobowiązkowa deklaracja publicznych właściwości
-  name;
-  // Obowiązkowa deklaracja prywatnych właściwości
-  #email;
-
-  constructor({ name, email }) {
-    this.name = name;
-    this.#email = email;
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value + str;
+  }
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
   }
 }
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
